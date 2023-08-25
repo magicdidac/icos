dd890f99f71c19a93d94b1018617528e351d5b93
dpadilla <didacpadilla20@gmail.com>
2023-08-25 19:02:06 +0200

    added basic-ftp

--- package/src/core/uploadImage.ts
+++ package/src/core/uploadImage.ts
@@ -1,29 +1,28 @@
-import sharp from "sharp";
+// import sharp from "sharp";
 import { IImageFile } from "../types";
-// import { Client } from "basic-ftp";
+import { Client } from "basic-ftp";
 import { Readable } from "stream";
 // import axios from 'axios';
 
-// const ftpClient = new Client()
+const ftpClient = new Client()
 
 export default async (route: string, image: IImageFile): Promise<string> => {
   if (!route.startsWith('/images/')) throw 'The route must start with `/images/`'
   if (!route.endsWith('/')) throw 'The route must end with `/`'
   const imageName = image.name.split('.')[0] + '.webp'
   const imageUrl = `https://icos.magicdidac.com${route}${imageName}`
-  const imageBuffer = await sharp(image.url).webp({ quality: 50 }).toBuffer()
+  // const imageBuffer = await sharp(image.url).webp({ quality: 50 }).toBuffer()
+
+  await ftpClient.access({
+    host: 'ftp.magicdidac.com',
+    user: process.env.FTP_ACCOUNT,
+    password: process.env.FTP_PASSWORD,
+    secure: false
+  })
+  await ftpClient.uploadFrom(Readable.from(new Buffer('Hello')), route + imageName)
+  ftpClient.close()
 
   return 'Hello'
-
-  // await ftpClient.access({
-  //   host: 'ftp.magicdidac.com',
-  //   user: process.env.FTP_ACCOUNT,
-  //   password: process.env.FTP_PASSWORD,
-  //   secure: false
-  // })
-  // await ftpClient.uploadFrom(Readable.from(imageBuffer), route + imageName)
-  // ftpClient.close()
-
   // // upload to db with gql
   // const data = JSON.stringify({
   //   query: `mutation ($route: String!, $name: String!, $url: String!) {
