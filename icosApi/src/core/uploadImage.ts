dca6f8f358f0c0546032084352d14a24c4daf2bc
dpadilla <didacpadilla20@gmail.com>
2023-08-30 11:58:14 +0200

    Added all logic to upload

--- icosApi/src/core/uploadImage.ts
+++ icosApi/src/core/uploadImage.ts
@@ -1,3 +1,28 @@
-export default async (route: string, name: string, image: any) => {
+import sharp = require("sharp")
+import { failure, success } from "../utils"
+import { Client } from "basic-ftp"
+import { Readable } from "stream"
 
+const ftpClient = new Client()
+
+export default async (route: string, name: string, image: string) => {
+  if (!route.startsWith('/images/')) return failure({ message: 'The route must start with `/images/`' })
+  if (!route.endsWith('/')) return failure({ message: 'The route must end with `/`' })
+
+  const imageName = name.split('.')[0] + '.webp'
+  const completeRoute = route + imageName
+  const imageUrl = `https://icos.magicdidac.com${completeRoute}`
+  const imageBuffer = await sharp(image).webp({ quality: 50 }).toBuffer()
+
+  await ftpClient.access({
+    host: 'ftp.magicdidac.com',
+    user: process.env.FTP_ACCOUNT,
+    password: process.env.FTP_PASSWORD,
+    secure: false
+  })
+
+  await ftpClient.uploadFrom(Readable.from(imageBuffer), completeRoute)
+  ftpClient.close()
+
+  return success({ url: imageUrl })
 }
--- icosApi/src/index.ts
+++ icosApi/src/index.ts
@@ -1,23 +1,22 @@
+import { uploadImage } from "./core"
 import { failure, notFound, success } from "./utils"
 
 export const handler = async (event) => {
-  console.log('Event:', JSON.stringify(event))
-  console.log('Type:', typeof event.body)
-  return success({})
-  // try {
-  //   switch (event.httpMethod) {
-  //     case 'POST':
-  //       if (event.body && event.body.name && event.body.image) {
-  //         await uploadImage(event.body.name, event.body.image)
-  //         return success({})
-  //       }
-  //       return failure({ message: 'You must provide `body: { name, image }`' })
-  //     default:
-  //       console.error(`Http method (${event.httpMethod}) not supported`)
-  //       return notFound({ message: 'Request type not defined' })
-  //   }
-  // } catch (error) {
-  //   console.error('INTERNAL ERROR IN LAMBDA', error)
-  //   return failure({ message: 'Error. Try again later.' })
-  // }
+  console.log('EVENT:', JSON.stringify(event))
+
+  try {
+    switch (event.httpMethod) {
+      case 'POST':
+        if (event.body && event.body.route && event.body.name && event.body.image) {
+          return await uploadImage(event.body.route, event.body.name, event.body.image)
+        }
+        return failure({ message: 'You must provide `body: { route, name, image }`' })
+      default:
+        console.error(`Http method (${event.httpMethod}) not supported`)
+        return notFound({ message: 'Request type not defined' })
+    }
+  } catch (error) {
+    console.error('INTERNAL ERROR IN LAMBDA', error)
+    return failure({ message: 'Error. Try again later.' })
+  }
 }
