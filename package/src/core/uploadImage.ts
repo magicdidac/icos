2f6f5b2cbbe12e476fe64f7e02267415299e5c35
dpadilla <didacpadilla20@gmail.com>
2023-09-10 02:27:57 +0200

    Added call to package

--- package/src/core/uploadImage.ts
+++ package/src/core/uploadImage.ts
@@ -1,23 +1,20 @@
-// import sharp from "sharp";
 import { IImageFile } from "../types";
 import axios from 'axios';
-import Client from 'ftp-ts';
+import fs from 'fs';
 
 export default async (route: string, image: IImageFile): Promise<string> => {
   if (!route.startsWith('/images/')) throw 'The route must start with `/images/`'
   if (!route.endsWith('/')) throw 'The route must end with `/`'
   const imageName = image.name.split('.')[0] + '.webp'
   const imageUrl = `https://icos.magicdidac.com${route}${imageName}`
-  // const imageBuffer = await sharp(image.url).webp({ quality: 50 }).toBuffer()
 
-  const ftpConnection = await Client.connect({
-    host: 'ftp.magicdidac.com',
-    user: process.env.FTP_ACCOUNT,
-    password: process.env.FTP_PASSWORD,
-    secure: false
+  const response = await axios.post('https://joaxmvql14.execute-api.us-east-1.amazonaws.com/prod/upload', {
+    name: image.name,
+    route: route,
+    image: fs.createReadStream(image.url)
   })
-  await ftpConnection.put(new Buffer('Hello'), route + imageName)
-  ftpConnection.end()
+
+  console.log(response.data)
 
   // upload to db with gql
   const data = JSON.stringify({
